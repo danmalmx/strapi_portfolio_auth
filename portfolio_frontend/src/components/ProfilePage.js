dbfc46939ceeb670dd8ec0c25f2558efec4bee3f
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2021-03-27 14:24:58 +0100

    Added functionality to update user info

--- portfolio_frontend/src/components/ProfilePage.js
+++ portfolio_frontend/src/components/ProfilePage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 import { handleChange } from '../utils/inputs'
+import { API_URL } from '../utils/urls'
 
 export class ProfilePage extends Component {
   constructor(props) {
@@ -14,13 +16,43 @@ export class ProfilePage extends Component {
       }
   }
 
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const { bio, oneLiner } = this.state;
+
+    const data = {
+      bio,
+      favorite_one_liner: oneLiner,
+    }
+
+    const userId = this.props.user.user.id;
+    const jwtToken = this.props.user.jwt;
+
+    const updateUserRes = await axios({
+      method: 'PUT',
+      url: `${API_URL}/users/${userId}`,
+      data,
+      headers: {
+        Authorization: `Bearer ${jwtToken}`
+      }
+    })
+
+    console.log('updateUserRes', updateUserRes);
+
+  }
 
   componentDidMount() {
-    console.log('this', this);
+    console.log('this.props.use', this.props.user);
+
+    const { bio, favorite_one_liner } = this.props.user.user;
+
+    this.setState({bio, oneLiner: favorite_one_liner});
 
   }
   render() {
     const { bio, oneLiner} = this.state;
+
     return (
       <div className="ProfilePage">
         Profile Page
@@ -32,6 +64,7 @@ export class ProfilePage extends Component {
           <label htmlFor="oneLiner">One Liner</label>
           <input type="text" name="oneLiner" value={oneLiner} onChange={this.handleChange}/>
         </div>
+        <button type="submit" onClick={this.handleSubmit}>Update your profile</button>
       </div>
     )
   }
